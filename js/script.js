f971ee4b8bd3a9af8a54d950a6f3d55a53974275
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-05-03 18:01:37 +0200

    add scaffholding and imported vue

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ Descrizione
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista con Vue JS e Axios.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+ */
+
+console.log('JS OK!');
+console.log('Vue', Vue);
